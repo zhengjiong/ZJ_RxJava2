dcabbe2bf5c588baab013c5d52fc20851f73d6ca
zhengjiong <zhengjiong2006@gmail.com>
2018-06-29 13:10:39 +0800

    feat(error):add 异常捕获的坑

--- rxjava-test/src/main/java/com/example/backpressure/Example2_Flowable_BUFFER.java
+++ rxjava-test/src/main/java/com/example/backpressure/Example2_Flowable_BUFFER.java
@@ -29,7 +29,7 @@ public class Example2_Flowable_BUFFER {
     /**
      * 这次我们直接让上游发送了1000个事件,下游仍然不调用request去请求, 与之前不同的是, 这次我们用的策
      * 略是BackpressureStrategy.BUFFER, 这就是我们的新水缸啦, 这个水缸就比原来的水缸牛逼多了,如果
-     * 说原来的水缸是95式步枪, 那这个新的水缸就好比黄金AK , 它没有大小限制, 因此可以存放许许多多的事件.
+     * 说原来的水缸是95式步枪, 那这个新的水缸就好比黄金AK, 它没有大小限制, 因此可以存放许许多多的事件.
      *
      * 换了水缸的FLowable和Observable好像是一样的,不错, 这时的FLowable表现出来的特性的确
      * 和Observable一模一样, 因此, 如果你像这样单纯的使用Flowable, 同样需要注意OOM的问题.
---
+++ rxjava-test/src/main/java/com/example/error/ErrorTest.java
@@ -0,0 +1,102 @@
+package com.example.error;
+
+import java.io.IOException;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * 异常捕获的坑!
+ * CreateTime:18/6/22  13:57
+ *
+ * @author 郑炯
+ * @version 1.0
+ */
+public class ErrorTest {
+
+    public static void main(String[] args){
+        //test1();
+        test2();
+
+    }
+
+    /**
+     * 如果subscribe的时候传入的参数是Observer, 那么在onNext中如果抛出异常,
+     * onError就会不能捕获, 程序直接报错.
+     */
+    private static void test1() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+            }
+        }).subscribeOn(Schedulers.newThread()).observeOn(Schedulers.newThread()).subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("onNext " + integer);
+                //抛出异常后, onError不能捕获
+                throw new NullPointerException("zhengjiong");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError -> " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        });
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void test2() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.newThread()).observeOn(Schedulers.newThread()).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                System.out.println("onNext " + integer);
+                //抛出异常onError可以捕获
+                throw new NullPointerException("zhengjiong");
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                System.out.println("onError" + throwable.getMessage());
+            }
+        }, new Action() {
+            @Override
+            public void run() throws Exception {
+                System.out.println("onComplete");
+            }
+        });
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
